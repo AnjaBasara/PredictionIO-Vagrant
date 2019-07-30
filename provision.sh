61fc59faaff53d4b759bc6742cceed3b0a916e64
AnjaBasara <anjaxbasara@gmail.com>
2019-07-30 11:40:43 +0200

    Changed installation script path

--- provision.sh
+++ provision.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 SETUP_DIR=$HOME/.pio
-INSTALLED_FLAG=$SETUP_DIR/installed
+INSTALL_SCRIPT_PATH='https://raw.githubusercontent.com/AnjaBasara/predictionio/develop/bin/install.sh'
 
 mkdir -p $SETUP_DIR
 
-if [ ! -f $INSTALLED_FLAG ]; then
+if [ ! -x "$(command -v pio-start-all)" ]; then
 
   echo "Installing PredictionIO..."
-  bash -e -c "$(curl -s https://install.prediction.io/install.sh)" 0 -y
+  bash -e -c "$(curl -sS $INSTALL_SCRIPT_PATH)" 0 -y
   if [ $? -ne 0 ]; then
 
     echo "ERROR: PredictionIO installation failed."
@@ -16,24 +16,21 @@ if [ ! -f $INSTALLED_FLAG ]; then
     echo "ERROR: $ vagrant destroy"
     echo "ERROR: (enter y) followed by"
     echo "ERROR: $ vagrant up"
-    echo "ERROR: If problem persists, please use this forum for support:"
-    echo "ERROR: https://groups.google.com/forum/#!forum/predictionio-user"
     exit 1
 
   else
 
     echo "Finish PredictionIO installation."
-    touch $INSTALLED_FLAG
 
   fi
 
 else
+  echo "--------------------------------------------------------------------------------"
   echo "PredictionIO already installed. Skip installation."
+  echo "--------------------------------------------------------------------------------"
   pio-start-all
   echo "--------------------------------------------------------------------------------"
   echo -e "\033[1;32mPredictionIO VM is up!\033[0m"
-  echo "You could run 'pio status' inside VM ('vagrant ssh' to VM first) to confirm if PredictionIO is ready."
-  echo -e "\033[1;33mIMPORTANT: You still have to start the eventserver manually (inside VM):\033[0m"
-  echo -e "Run: '\033[1mpio eventserver --ip 0.0.0.0\033[0m'"
+  echo -e "\033[1;33mYou could run 'pio status' inside VM ('vagrant ssh' to VM first) to confirm if PredictionIO is ready.\033[0m"
   echo "--------------------------------------------------------------------------------"
 fi
